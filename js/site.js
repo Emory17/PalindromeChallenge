62011699674953984f4c93ce582bfae38edbfb82
Emory Soper <emorysoper@icloud.com>
2023-04-11 17:46:01 -0400

    ignore whitespace

--- js/site.js
+++ js/site.js
@@ -1,11 +1,41 @@
-function displayMessage() {
-    let msg = document.getElementById("message").value;
-    //alert(msg);
-    Swal.fire(
+function checkPalindrome() {
+    let val = document.getElementById("inputstr").value;
+    
+    if(val.length==0){return;}
+
+    str = val.toLowerCase().trim();
+
+    let pal = true;
+    let m = 0;
+    let n = str.length-1
+
+    while(m < n){
+        if(str[m] != str[n])
         {
-            backdrop: false,
-            title: 'APP NAME',
-            text: msg
+            pal = false;
+            break;
         }
-    );
+        m++;
+        while(str[m] == ' '){m++;}
+        n--;
+        while(str[n] == ' '){n--;}
+    }
+
+    if(pal){
+        Swal.fire(
+            {
+                backdrop: false,
+                title: 'APP NAME',
+                text: str + " is a palindrome!"
+            }
+        );
+    }
+    else{
+        Swal.fire(
+            {
+            backdrop: false,
+            title: "APP NAME",
+            text: str + " is NOT a palindrome.",
+        });
+    }
 }
